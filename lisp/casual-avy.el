f9327702da039fee6c1d708f0335a7561348c097
Charles Choi <kickingvegas@gmail.com>
2024-08-27 19:21:52 -0700

    Bump version to 1.4.2

--- lisp/casual-avy.el
+++ lisp/casual-avy.el
@@ -5,7 +5,7 @@
 ;; Author: Charles Choi <kickingvegas@gmail.com>
 ;; URL: https://github.com/kickingvegas/casual-avy
 ;; Keywords: tools
-;; Version: 1.4.1
+;; Version: 1.4.2
 ;; Package-Requires: ((emacs "29.1") (avy "0.5.0") (casual-lib "1.1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
